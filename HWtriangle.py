eb2ba385f492fb94d7d484b67830dd391ecc9807
Iryna Zharkova <gorbatyk83@gmail.com>
2023-07-30 00:03:51 -0500

    HWtriangle.py file was modifed

---
+++ HWtriangle.py
@@ -0,0 +1,14 @@
+Python 3.10.8 (v3.10.8:aaaf517424, Oct 11 2022, 10:14:40) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("------")
+... 
+... print_this_line = "|   /"
+... print(print_this_line)
+... 
+... print_this_line = "|  /"
+... print(print_this_line)
+... 
+... print_this_line = "| /"
+... print(print_this_line)
+... 
+... print_this_line = "|/"
